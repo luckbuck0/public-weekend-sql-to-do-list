a73f738f0005912b36445508c5b0d95b4d6dee07
Luckie Bah <bah966@gmail.com>
2023-04-18 11:03:51 +0200

    connected all the files together and made sure the files were working together

--- server/routes/todoroutes.js
+++ server/routes/todoroutes.js
@@ -1,2 +1,22 @@
 const router = require('express').Router();
 const pool = require('/Users/LuckieBah/Documents/Coding/wk9/full-stack-treats/server/modules/pool');
+
+router.get('/', (req, res) => {
+    
+    let sqlText = 'SELECT * FROM "toDoList";';
+  
+
+    pool.query(sqlText)
+      .then((dbRes) => {
+        let toDoList = dbRes.rows;
+        res.send(toDoList)
+      })
+      .catch((dbErr) => {
+        console.log('SQL query in GET /list failed:', dbErr)
+        res.sendStatus(500);
+      })
+   
+  })
+
+
+  module.exports = router;
